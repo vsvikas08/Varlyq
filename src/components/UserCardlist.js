4f1ebeb5e9945befe59df3bb8fd28f16ea366744
Vikas Kumar <vikas3517@gmail.com>
2021-11-16 01:34:42 +0530

    Search option implemented

--- src/components/UserCardlist.js
+++ src/components/UserCardlist.js
@@ -1,9 +1,10 @@
-import { Grid, TextField, Typography } from '@mui/material';
+import { Button, FormControl, FormHelperText, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { Box } from '@mui/system';
 import React from 'react';
 import UserCard from './UserCard';
 import Divider from '@mui/material/Divider';
+import { useState } from 'react';
 
 const useStyles = makeStyles({
     gridContainer: {
@@ -16,6 +17,8 @@ const UserCardlist = (props) => {
     const classes = useStyles();
     const {employee} = props.state;
     console.log('Card STATE',employee);
+
+    const [searchTerm,setSearchTerm] = useState("");
     return (
         <Box>
             <Typography variant="h2" sx={{m: 3}}>
@@ -23,15 +26,58 @@ const UserCardlist = (props) => {
             </Typography>
             <Divider sx={{m: 3}}/>
 
-            {/* <TextField
-                id="outlined-name"
+            <TextField
+                id="search"
                 label="Name"
                 size="small"
-                sx={{mb:2,ml:3}}
-            /> */}
-
+                sx={{mb:2,ml:3,mr: 4}}
+                onChange={(event)=>{
+                    setSearchTerm(event.target.value)
+                }}
+            />
+            <Box sx={{float: 'right'}}>
+            <FormControl sx={{ mb: 2, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">All</InputLabel>
+                <Select
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select-helper"
+                label="All"
+                size="small"
+                >
+                <MenuItem value="">
+                    <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl sx={{fmb: 2,ml: 4, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">Default</InputLabel>
+                <Select
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select-helper"
+                label="All"
+                size="small"
+                >
+                <MenuItem value="">
+                    <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+            <Button sx={{float: 'right', mr: 3,ml: 3}} variant="contained">Add Employee</Button>
+            </Box>
             <Grid container spacing={4} className={classes.gridContainer}>
-                {employee.map((emp) => {
+                {employee.filter((val)=>{
+                    if(searchTerm == ""){
+                        return val
+                    }else if(val.name.toLowerCase().includes(searchTerm.toLowerCase())){
+                        return val
+                    }
+                }).map((emp) => {
                             return (
                                 <Grid item xs={12} sm={6} md={4} lg={3}>
                                     <UserCard employee={emp} key={emp.id} />
